1ac8cca90a9bb9039e18b453e6a0ddf38b50d8c8
Christopher Baron <topherbaron@gmail.com>
2017-11-29 18:12:58 -0500

    bundle

---
+++ sql/schema/20171114-byop2017results.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "byop2017Results" (
+    "division"     VARCHAR(50),
+    "player1"     VARCHAR(100),
+    "player2"     VARCHAR(100),
+    "rd1"        VARCHAR(20),
+    "rd2"        VARCHAR(20),
+    "final"      VARCHAR(20),
+    "1"          VARCHAR(50),
+    "2"          VARCHAR(50),
+    "3"          VARCHAR(50),
+    "4"          VARCHAR(50),
+    "5"          VARCHAR(50),
+    "6"          VARCHAR(50),
+    "7"          VARCHAR(50),
+    "8"          VARCHAR(50)
+);
