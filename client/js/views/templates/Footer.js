640f9dccc5c5428e98220990f3632d05723e74eb
ScottAP108 <saparton@gmail.com>
2017-12-01 17:14:43 -0800

    putting 2nd hazy address back in footer

--- client/js/views/templates/Footer.js
+++ client/js/views/templates/Footer.js
@@ -8,8 +8,10 @@ module.exports = p =>
                 <div>Hazy Shade Disc Golf</div>
                 <div>723 Watervliet Ave</div>
                 <div>Dayton, OH 45420</div>
+                <div>2113 Park Rd</div>
+                <div>Springfield, OH 45504</div>
                 <div>(937) 256-2690</div>
-                <div><a href="mailto:sales@hazyshade.com">sales@hazyshade.com</a></div>
+                <div>sales@hazyshade.com</div>
                 <div>Mon &ndash; Sat 11am &ndash; 8pm</div>
                 <div>Sun 12pm &ndash; 7pm</div>
                 <a href="https://www.facebook.com/Hazy-Shade-Disc-Golf-And-More-173084619405424" target="_blank">${require('./lib/facebook')()}</a>
